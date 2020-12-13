88c6fbbaf6c0703e61069412304765a1abbb3648
Josué Louis-Alexandre <jolouisalexandre@gmail.com>
2020-12-13 16:17:56 +0100

    streamlit app

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
 import streamlit as st 
+
+
+st.title("PortraitGAN - Selfie to Portrait GAN Model")
+st.header("In this ") 
+
+slider = st.slider("Slider title", 0, 100, 50)
+check = st.checkbox("Selfie 2 Portrait")
