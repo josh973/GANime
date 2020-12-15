beefbae41320573f738369ea1d05df9d98440813
Josué Louis-Alexandre <jolouisalexandre@gmail.com>
2020-12-15 22:56:25 +0100

    final product

--- streamlit.py
+++ streamlit.py
@@ -1,29 +1,280 @@
+# Streamlit import 
 import streamlit as st 
-from PIL import Image
 import os
 
 
+#import model
+import sys
+sys.path.append("../")
+from io import BytesIO
+#import model fat 
+import base64
+import requests
+import torch
+import os
+import numpy as np
+import argparse
+from PIL import Image
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+import torchvision.utils as vutils
+from tqdm import tqdm_notebook
+
+##Needed for test_from_code
+import time
+import torch
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+
+#from network.Transformer import Transformer
+import torch.nn as nn
+import torch.nn.functional as F
+
+#import needed class 
+class Transformer(nn.Module):
+    def __init__(self):
+        super(Transformer, self).__init__()
+        #
+        self.refpad01_1 = nn.ReflectionPad2d(3)
+        self.conv01_1 = nn.Conv2d(3, 64, 7)
+        self.in01_1 = InstanceNormalization(64)
+        # relu
+        self.conv02_1 = nn.Conv2d(64, 128, 3, 2, 1)
+        self.conv02_2 = nn.Conv2d(128, 128, 3, 1, 1)
+        self.in02_1 = InstanceNormalization(128)
+        # relu
+        self.conv03_1 = nn.Conv2d(128, 256, 3, 2, 1)
+        self.conv03_2 = nn.Conv2d(256, 256, 3, 1, 1)
+        self.in03_1 = InstanceNormalization(256)
+        # relu
+
+        # res block 1
+        self.refpad04_1 = nn.ReflectionPad2d(1)
+        self.conv04_1 = nn.Conv2d(256, 256, 3)
+        self.in04_1 = InstanceNormalization(256)
+        # relu
+        self.refpad04_2 = nn.ReflectionPad2d(1)
+        self.conv04_2 = nn.Conv2d(256, 256, 3)
+        self.in04_2 = InstanceNormalization(256)
+        # + input
+
+        # res block 2
+        self.refpad05_1 = nn.ReflectionPad2d(1)
+        self.conv05_1 = nn.Conv2d(256, 256, 3)
+        self.in05_1 = InstanceNormalization(256)
+        # relu
+        self.refpad05_2 = nn.ReflectionPad2d(1)
+        self.conv05_2 = nn.Conv2d(256, 256, 3)
+        self.in05_2 = InstanceNormalization(256)
+        # + input
+
+        # res block 3
+        self.refpad06_1 = nn.ReflectionPad2d(1)
+        self.conv06_1 = nn.Conv2d(256, 256, 3)
+        self.in06_1 = InstanceNormalization(256)
+        # relu
+        self.refpad06_2 = nn.ReflectionPad2d(1)
+        self.conv06_2 = nn.Conv2d(256, 256, 3)
+        self.in06_2 = InstanceNormalization(256)
+        # + input
+
+        # res block 4
+        self.refpad07_1 = nn.ReflectionPad2d(1)
+        self.conv07_1 = nn.Conv2d(256, 256, 3)
+        self.in07_1 = InstanceNormalization(256)
+        # relu
+        self.refpad07_2 = nn.ReflectionPad2d(1)
+        self.conv07_2 = nn.Conv2d(256, 256, 3)
+        self.in07_2 = InstanceNormalization(256)
+        # + input
+
+        # res block 5
+        self.refpad08_1 = nn.ReflectionPad2d(1)
+        self.conv08_1 = nn.Conv2d(256, 256, 3)
+        self.in08_1 = InstanceNormalization(256)
+        # relu
+        self.refpad08_2 = nn.ReflectionPad2d(1)
+        self.conv08_2 = nn.Conv2d(256, 256, 3)
+        self.in08_2 = InstanceNormalization(256)
+        # + input
+
+        # res block 6
+        self.refpad09_1 = nn.ReflectionPad2d(1)
+        self.conv09_1 = nn.Conv2d(256, 256, 3)
+        self.in09_1 = InstanceNormalization(256)
+        # relu
+        self.refpad09_2 = nn.ReflectionPad2d(1)
+        self.conv09_2 = nn.Conv2d(256, 256, 3)
+        self.in09_2 = InstanceNormalization(256)
+        # + input
+
+        # res block 7
+        self.refpad10_1 = nn.ReflectionPad2d(1)
+        self.conv10_1 = nn.Conv2d(256, 256, 3)
+        self.in10_1 = InstanceNormalization(256)
+        # relu
+        self.refpad10_2 = nn.ReflectionPad2d(1)
+        self.conv10_2 = nn.Conv2d(256, 256, 3)
+        self.in10_2 = InstanceNormalization(256)
+        # + input
+
+        # res block 8
+        self.refpad11_1 = nn.ReflectionPad2d(1)
+        self.conv11_1 = nn.Conv2d(256, 256, 3)
+        self.in11_1 = InstanceNormalization(256)
+        # relu
+        self.refpad11_2 = nn.ReflectionPad2d(1)
+        self.conv11_2 = nn.Conv2d(256, 256, 3)
+        self.in11_2 = InstanceNormalization(256)
+        # + input
+
+        ##------------------------------------##
+        self.deconv01_1 = nn.ConvTranspose2d(256, 128, 3, 2, 1, 1)
+        self.deconv01_2 = nn.Conv2d(128, 128, 3, 1, 1)
+        self.in12_1 = InstanceNormalization(128)
+        # relu
+        self.deconv02_1 = nn.ConvTranspose2d(128, 64, 3, 2, 1, 1)
+        self.deconv02_2 = nn.Conv2d(64, 64, 3, 1, 1)
+        self.in13_1 = InstanceNormalization(64)
+        # relu
+        self.refpad12_1 = nn.ReflectionPad2d(3)
+        self.deconv03_1 = nn.Conv2d(64, 3, 7)
+        # tanh
+
+    def forward(self, x):
+        y = F.relu(self.in01_1(self.conv01_1(self.refpad01_1(x))))
+        y = F.relu(self.in02_1(self.conv02_2(self.conv02_1(y))))
+        t04 = F.relu(self.in03_1(self.conv03_2(self.conv03_1(y))))
+
+        ##
+        y = F.relu(self.in04_1(self.conv04_1(self.refpad04_1(t04))))
+        t05 = self.in04_2(self.conv04_2(self.refpad04_2(y))) + t04
+
+        y = F.relu(self.in05_1(self.conv05_1(self.refpad05_1(t05))))
+        t06 = self.in05_2(self.conv05_2(self.refpad05_2(y))) + t05
+
+        y = F.relu(self.in06_1(self.conv06_1(self.refpad06_1(t06))))
+        t07 = self.in06_2(self.conv06_2(self.refpad06_2(y))) + t06
+
+        y = F.relu(self.in07_1(self.conv07_1(self.refpad07_1(t07))))
+        t08 = self.in07_2(self.conv07_2(self.refpad07_2(y))) + t07
+
+        y = F.relu(self.in08_1(self.conv08_1(self.refpad08_1(t08))))
+        t09 = self.in08_2(self.conv08_2(self.refpad08_2(y))) + t08
+
+        y = F.relu(self.in09_1(self.conv09_1(self.refpad09_1(t09))))
+        t10 = self.in09_2(self.conv09_2(self.refpad09_2(y))) + t09
+
+        y = F.relu(self.in10_1(self.conv10_1(self.refpad10_1(t10))))
+        t11 = self.in10_2(self.conv10_2(self.refpad10_2(y))) + t10
+
+        y = F.relu(self.in11_1(self.conv11_1(self.refpad11_1(t11))))
+        y = self.in11_2(self.conv11_2(self.refpad11_2(y))) + t11
+        ##
+
+        y = F.relu(self.in12_1(self.deconv01_2(self.deconv01_1(y))))
+        y = F.relu(self.in13_1(self.deconv02_2(self.deconv02_1(y))))
+        y = F.tanh(self.deconv03_1(self.refpad12_1(y)))
+
+        return y
+
+
+class InstanceNormalization(nn.Module):
+    def __init__(self, dim, eps=1e-9):
+        super(InstanceNormalization, self).__init__()
+        self.scale = nn.Parameter(torch.FloatTensor(dim))
+        self.shift = nn.Parameter(torch.FloatTensor(dim))
+        self.eps = eps
+        self._reset_parameters()
+
+    def _reset_parameters(self):
+        self.scale.data.uniform_()
+        self.shift.data.zero_()
+
+    def __call__(self, x):
+        n = x.size(2) * x.size(3)
+        t = x.view(x.size(0), x.size(1), n)
+        mean = torch.mean(t, 2).unsqueeze(2).unsqueeze(3).expand_as(x)
+        # Calculate the biased var. torch.var returns unbiased var
+        var = torch.var(t, 2).unsqueeze(2).unsqueeze(
+            3).expand_as(x) * ((n - 1) / float(n))
+        scale_broadcast = self.scale.unsqueeze(1).unsqueeze(1).unsqueeze(0)
+        scale_broadcast = scale_broadcast.expand_as(x)
+        shift_broadcast = self.shift.unsqueeze(1).unsqueeze(1).unsqueeze(0)
+        shift_broadcast = shift_broadcast.expand_as(x)
+        out = (x - mean) / torch.sqrt(var + self.eps)
+        out = out * scale_broadcast + shift_broadcast
+        return out
+
+#import the transform function
+
+def transform(models, style, input, load_size=450, gpu=-1):
+    model = models[style]
+
+    if gpu > -1:
+        model.cuda()
+    else:
+        model.float()
+
+    input_image = input.convert("RGB")
+    #input_image = Image.open(input).convert("RGB")
+    h, w = input_image.size
+
+    ratio = h * 1.0 / w
+
+    if ratio > 1:
+        h = load_size
+        w = int(h * 1.0 / ratio)
+    else:
+        w = load_size
+        h = int(w * ratio)
+
+    input_image = input_image.resize((h, w), Image.BICUBIC)
+    input_image = np.asarray(input_image)
+
+    input_image = input_image[:, :, [2, 1, 0]]
+    input_image = transforms.ToTensor()(input_image).unsqueeze(0)
+
+    input_image = -1 + 2 * input_image
+    if gpu > -1:
+        input_image = Variable(input_image).cuda()
+    else:
+        input_image = Variable(input_image).float()
+
+    t0 = time.time()
+    with torch.no_grad():
+        output_image = model(input_image)[0]
+    print(f"inference time took {time.time() - t0} s")
+
+    output_image = output_image[[2, 1, 0], :, :]
+    output_image = output_image.data.cpu().float() * 0.5 + 0.5
+
+    output_image = output_image.numpy()
+
+    output_image = np.uint8(output_image.transpose(1, 2, 0) * 255)
+    output_image = Image.fromarray(output_image)
+
+    return output_image
+
+styles = ["Hosoda", "Hayao", "Shinkai", "Paprika"]
+
+models = {}
+
+for style in tqdm_notebook(styles):
+    model = Transformer()
+    model.load_state_dict(torch.load(os.path.join("./pretrained_models/", style + '_net_G_float.pth')))
+    model.eval()
+    models[style] = model
 
 
 st.title("JapanSelfieGAN")
-st.header("From Selfie to Japan drawing style Portrait with GAN Model") 
+st.header("From any Selfie to Japanese Portrait powered by GAN Model") 
 st.markdown("")
 
 
 
-#path 
-"""
-path = os.path.join("selfie","JosuéLouis-Alexandre_INVC21HS-min.jpg")
-path2 = os.path.join("selfie","AhmedDeyab_INVC21HS.jpg")
-path3 = os.path.join("selfie","Anais_Cisneros_INVC21HS.png")
-path4 = os.path.join("selfie","IMG_20190920_152402_706.jpg")
-path5 = os.path.join("selfie","ShaunaJin_INVC21HS.jpg")
 
-#showing img 
-#img = Image.open(path)
-#img = img.resize((400,400))
-st.image(img)
-"""
+
 #file uploader
 st.header('Please provide a selfie to transform')
 upload_files = st.file_uploader('', accept_multiple_files=False)
@@ -31,7 +282,21 @@ upload_files = st.file_uploader('', accept_multiple_files=False)
 if upload_files is not None: 
     uploaded_img = Image.open(upload_files)
     uploaded_img = uploaded_img.resize((400,400))
+    path = uploaded_img
     st.image(uploaded_img)
 
+    
 
+# now we we transform the img 
+### pick a style in : ["Hosoda", "Hayao", "Shinkai", "Paprika"]
+st.header('Please choose a painting style:')
+
+style = st.radio('Style', ["Shinkai", "Hayao", "Paprika", "Hosoda"])
+
+st.header('Please choose load size (size of the picture):')
+load_size = int(st.select_slider('Slide to select', options=['300','400','500','600']))
+
+if upload_files is not None:
+    output = transform(models, style, path, load_size)
+    st.image(output)
 
