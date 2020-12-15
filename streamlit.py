8c367411b68490a80d38ef1e7395e3d041e04900
Josué Louis-Alexandre <jolouisalexandre@gmail.com>
2020-12-15 16:41:35 +0100

    push to the limit

--- streamlit.py
+++ streamlit.py
@@ -12,6 +12,7 @@ st.markdown("")
 
 
 #path 
+"""
 path = os.path.join("selfie","JosuéLouis-Alexandre_INVC21HS-min.jpg")
 path2 = os.path.join("selfie","AhmedDeyab_INVC21HS.jpg")
 path3 = os.path.join("selfie","Anais_Cisneros_INVC21HS.png")
@@ -19,10 +20,10 @@ path4 = os.path.join("selfie","IMG_20190920_152402_706.jpg")
 path5 = os.path.join("selfie","ShaunaJin_INVC21HS.jpg")
 
 #showing img 
-img = Image.open(path)
-img = img.resize((400,400))
+#img = Image.open(path)
+#img = img.resize((400,400))
 st.image(img)
-
+"""
 #file uploader
 st.header('Please provide a selfie to transform')
 upload_files = st.file_uploader('', accept_multiple_files=False)
@@ -31,4 +32,6 @@ if upload_files is not None:
     uploaded_img = Image.open(upload_files)
     uploaded_img = uploaded_img.resize((400,400))
     st.image(uploaded_img)
+
+
 
